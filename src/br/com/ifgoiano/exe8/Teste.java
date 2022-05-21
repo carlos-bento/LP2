d0add956ad6cbe18dccaccd048141421a41816e4
MarcosPauloSR <marcospaulosrezende@gmail.com>
2022-05-21 19:09:18 -0300

    Exércicio 8

--- src/br/com/ifgoiano/exe8/Teste.java
+++ src/br/com/ifgoiano/exe8/Teste.java
@@ -1,7 +1,21 @@
 package br.com.ifgoiano.exe8;
 
+import java.util.Scanner;
+
 public class Teste {
 
     public static void main(String[] args) {
 
-    }}
+        Scanner scan = new Scanner ( System.in );
+        Integer valor;
+
+        System.out.println ( "Digite um número qualquer" );
+        valor = scan.nextInt ( );
+
+        System.out.println ( "A tabuada do número: " + valor + " é: " );
+        for (int i = 1; i <= 10; i++) {
+            System.out.println ( valor + " x " + i + " = " + valor * i );
+        }
+    }
+
+}
