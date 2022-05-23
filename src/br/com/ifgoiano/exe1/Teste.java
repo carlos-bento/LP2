6f574e7ec8b442612da0db522ceededbfba1f167
Carlos Cesar Nunes Bento <carlos.bento@polichat.com.br>
2022-05-23 18:16:44 -0300

    Exercicio 01 + prettier changes

--- src/br/com/ifgoiano/exe1/Teste.java
+++ src/br/com/ifgoiano/exe1/Teste.java
@@ -1,11 +1,25 @@
 package br.com.ifgoiano.exe1;
 
+import java.util.Scanner;
+
 public class Teste {
+
+    // 1. Faça um programa para calcular o estoque médio de uma peça, sendo que:
+    // 𝑒𝑠𝑡𝑜𝑞𝑢𝑒𝑀𝑒𝑑𝑖𝑜 = (𝑞𝑢𝑎𝑛𝑡𝑖𝑑𝑎𝑑𝑒𝑀𝑖𝑛𝑖𝑚𝑎 +
+    // 𝑞𝑢𝑎𝑛𝑡𝑖𝑑𝑎𝑑𝑒𝑀𝑎𝑥𝑖𝑚𝑎)/2. Receba as quantidades via teclado.
 
     public static void main(String[] args) {
 
-        System.out.println ( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
 
+        Double estoqueMedio, quantidadeMinima, quantidadeMaxima;
+
+        System.out.println("Digite a quantidade mínima: ");
+        quantidadeMinima = scanner.nextDouble();
+        System.out.println("Digite a quantidade máxima: ");
+        quantidadeMaxima = scanner.nextDouble();
+
+        estoqueMedio = (quantidadeMinima + quantidadeMaxima) / 2;
+        System.out.println("O estoque médio é de: R$" + estoqueMedio);
     }
 }
-
--- src/br/com/ifgoiano/exe2/Teste.java
+++ src/br/com/ifgoiano/exe2/Teste.java
@@ -2,19 +2,25 @@ package br.com.ifgoiano.exe2;
 
 import java.util.Scanner;
 
+// 2. Faça um programa que:
+// ●   Leia a cotação do dólar
+// ●   Leia um valor em dólares 
+// ●   Converta esse valor para Real 
+// ●   Mostre o resultado
+
 public class Teste {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner ( System.in );
+        Scanner sc = new Scanner(System.in);
         Double cotacao, valorInicial, valorFinal;
-        System.out.println ( "Digite a Cotação do Dólar: " );
-        cotacao = sc.nextDouble ( );
-        System.out.println ( "Digite o Valor em Dólar: " );
-        valorInicial = sc.nextDouble ( );
+        System.out.println("Digite a Cotação do Dólar: ");
+        cotacao = sc.nextDouble();
+        System.out.println("Digite o Valor em Dólar: ");
+        valorInicial = sc.nextDouble();
 
         valorFinal = valorInicial * cotacao;
-        System.out.println ( "Valor em Real: R$" + valorFinal );
+        System.out.println("Valor em Real: R$" + valorFinal);
     }
 
 }
