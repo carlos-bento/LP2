1954cf49b47646ee196af661b5e8d54b1c2602a6
Carlos Cesar Nunes Bento <carloscesar_nunes@hotmail.com>
2022-05-23 23:21:40 -0300

    commit teste

---
+++ src/br/com/ifgoiano/exe4/Teste.java
@@ -0,0 +1,9 @@
+package br.com.ifgoiano.exe4;
+
+// 4. Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos valores de modo que a variável A 
+// passe a possuir o valor da variável B, e a variável B passe a possuir o valor da variável A. Apresentar os 
+// valores trocados.
+
+public class Teste {
+
+}
