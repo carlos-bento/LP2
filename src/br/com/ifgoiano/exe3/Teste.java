fb8826588c6500754f9632b31f2f0448f24bedc0
Carlos Cesar Nunes Bento <carloscesar_nunes@hotmail.com>
2022-06-01 03:00:05 -0300

    exercicios 3 e 6

--- src/br/com/ifgoiano/exe3/Teste.java
+++ src/br/com/ifgoiano/exe3/Teste.java
@@ -1,43 +1,76 @@
 package br.com.ifgoiano.exe3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
+// 3. Escrever um programa que leia três números inteiros e mostre todos os relacionamentos de ordem 
+// existentes entre eles. Os relacionamentos possíveis  são: Igual, Não igual, Maior, Menor, Maior ou igual, 
+// Menor ou igual.
 
 public class Teste {
 
     public static void main(String[] args) {
 
-        int a, b, c;
         Scanner recebe = new Scanner(System.in);
-        boolean Igual = false, NaoIgual = false, Maior = false, Menor = false, MaiorOuIgual = false, MenorOuIgual = false;
+
+        int[] numeros = new int[3];
+
+        int igualValor = 0;
+        int naoigualValor = 0;
+        int maiorValor = 0;
+        int menorValor = 0;
+        int maiorouigualValor = 0;
+        int menorouigualValor = 0;
+
+        int checar = 0; // iniciando a variavel
+
         System.out.println("Verificador de relacionamentos");
 
-        System.out.println("Digite um valor: ");
-        a = recebe.nextInt();
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Informe o " + (i + 1) + " valor: ");
+            numeros[i] = recebe.nextInt();
+        }
 
-        System.out.println("Digite um valor: ");
-        b = recebe.nextInt();
+        igualValor = numeros[0];
+        naoigualValor = numeros[0];
+        menorValor = numeros[0];
+        maiorValor = numeros[0];
+        maiorouigualValor = numeros[0];
+        menorouigualValor = numeros[0];
 
-        System.out.println("Digite um valor: ");
-        c = recebe.nextInt();
+        for (int i = 0; i < numeros.length; i++) {
 
-        if (a == b) Igual = true;
-        if (a == c) Igual = true;
-        if (b == c) Igual = true;
-        if (a != b) NaoIgual = true;
-        if (a != c) NaoIgual = true;
-        if (b != c) NaoIgual = true;
-        if (a > b) Maior = true;
-        if (a > c) Maior = true;
-        if (b > c) Maior = true;
-        if (a < b) Menor = true;
-        if (a < c) Menor = true;
-        if (b < c) Menor = true;
+            checar += numeros[i];
 
-        System.out.println("Igual = " + Igual);
-        System.out.println("NaoIgual = " + NaoIgual);
-        System.out.println("Maior = " + Maior);
-        System.out.println("Menor = " + Menor);
+            if (igualValor == numeros[i])
+                igualValor = numeros[i];
+
+            if (naoigualValor != numeros[i])
+                naoigualValor = numeros[i];
+
+            if (menorValor > numeros[i])
+                menorValor = numeros[i];
+
+            if (maiorValor < numeros[i])
+                maiorValor = numeros[i];
+
+            if (maiorouigualValor <= numeros[i])
+                maiorouigualValor = numeros[i];
+
+            if (menorouigualValor >= numeros[i])
+                menorouigualValor = numeros[i];
+        }
+
+        System.out.println("");
+        System.out.println("Menor valor: " + menorValor);
+        System.out.println("Maior valor: " + maiorValor);
+        System.out.println("");
+        System.out.println("Valor maior ou igual aos outros: " + maiorouigualValor);
+        System.out.println("Valor menor ou igual aos outros: " + menorouigualValor);
+        System.out.println("");
+        System.out.println("Valores que sao iguais: " + igualValor);
+        System.out.println("Valores que nao sao iguais: " + naoigualValor);
 
     }
 }
--- src/br/com/ifgoiano/exe6/Teste.java
+++ src/br/com/ifgoiano/exe6/Teste.java
@@ -1,27 +1,71 @@
 package br.com.ifgoiano.exe6;
 
-import java.util.Arrays;
 import java.util.Scanner;
+
+// 6. Escrever um programa declarando três variáveis do tipo inteiro (a, b e c). Ler um valor maior que zero para cada variável (se o valor digitado não é válido, mostrar mensagem e ler novamente). Exibe o menor lido multiplicado pelo maior e o maior valor dividido pelo menor.
 
 public class Teste {
 
     public static void main(String[] args) {
-        Scanner receber = new Scanner(System.in);
-        Double[] valor = new Double[3];
-        System.out.println("Verificando valores");
+        try (Scanner receber = new Scanner(System.in)) {
 
-        for (int i = 0; i < 3; i++){
-            System.out.println("Digite um valor " + (i+1) + ":");
-            valor[i] = receber.nextDouble();
+            int a = 0, b = 0, c = 0;
+            int menorValor = 0;
+            int maiorValor = 0;
+            int checar = 0; // inicializando variavel
 
-            while (valor[i] <= 0){
-                System.out.println("Digite um valor maior que 0 " + (i+1) + ":");
-                valor[i] = receber.nextDouble();
+            System.out.println("Verificando valores");
+
+            // for (int i = 0; i < 3; i++) {
+            // System.out.println("Digite um valor " + (i + 1) + ":");
+            // valor[i] = receber.nextInt();
+
+            // while (valor[i] <= 0) {
+            // System.out.println("Digite um valor " + (i + 1) + " que seja maior do que
+            // 0:");
+            // valor[i] = receber.nextInt();
+            // }
+            // }
+
+            System.out.println("Digite um valor para A:");
+            a = receber.nextInt();
+            if (a <= 0) {
+                System.out.println("Digite um valor para A que seja maior do que 0:");
+                a = receber.nextInt();
             }
 
-        }
-        Arrays.sort(valor);
-        System.out.println("Maior valor multiplicado pelo o menor valor: "+valor[0]*valor[2]+" " + "Maior valor divido pelo menor valor: " +valor[0]/valor[2]);
-    }
+            System.out.println("Digite um valor para B:");
+            b = receber.nextInt();
+            if (b <= 0) {
+                System.out.println("Digite um valor para B que seja maior do que 0:");
+                b = receber.nextInt();
+            }
 
+            System.out.println("Digite um valor para C:");
+            c = receber.nextInt();
+            if (c <= 0) {
+                System.out.println("Digite um valor para C que seja maior do que 0:");
+                c = receber.nextInt();
+            }
+
+            int[] valor = { a, b, c };
+            menorValor = valor[0];
+            maiorValor = valor[0];
+
+            for (int i = 0; i < valor.length; i++) {
+                checar += valor[i];
+
+                if (menorValor > valor[i])
+                    menorValor = valor[i];
+
+                if (maiorValor < valor[i])
+                    maiorValor = valor[i];
+
+            }
+
+            System.out.println("Maior valor multiplicado pelo o menor valor: " + maiorValor * menorValor);
+            System.out.println("Maior valor divido pelo menor valor: " + maiorValor / menorValor);
+
+        }
+    }
 }
