4b7c0fb5c491fe95bc2f85b627edde49174c0237
MarcosPauloSR <marcospaulosrezende@gmail.com>
2022-06-11 20:08:05 -0300

    Exercício 14 - incompleto Exercício 17 - tem um pequeno bug na verificação da quantidade de valores

--- src/br/com/ifgoiano/exe14/exe14.java
+++ src/br/com/ifgoiano/exe14/exe14.java
@@ -1,6 +1,5 @@
 package br.com.ifgoiano.exe14;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class exe14 {
@@ -8,75 +7,46 @@ public class exe14 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner ( System.in );
-        ArrayList<Integer> numeros = new ArrayList<> ( );
-        String sair = "";
-        int aux = 0;
 
-        while ( !(sair).equalsIgnoreCase ( "S" ) ) {
-            aux++;
+        System.out.println ( "Digite a quantidade de números que deseja inserir:" );
+        Integer qtdNumeros = sc.nextInt ( );
 
-            System.out.println ( "Digite um número inteiro: " );
-            numeros.add ( sc.nextInt ( ) );
+        Integer[] numeros = new Integer[qtdNumeros];
 
-            if (aux >= 2) {
-                System.out.println ( "Deseja sair? (S para sair e N para não)" );
-                sair = sc.next ( );
-            }
+        for (int i = 0; i < qtdNumeros; i++) {
+            System.out.println ( "Digite o " + (i + 1) + "º número:" );
+            numeros[i] = sc.nextInt ( );
         }
 
-        //System.out.println ( (isSquareMatrix ( numeros )) );
-        printSquareMatrix ( numeros );
+        geraMatriz ( qtdNumeros, numeros );
+
 
     }
 
-    //create a method that will receive the arraylist and print square matrix
-    public static void printSquareMatrix(ArrayList<Integer> numeros) {
+    public static void geraMatriz(Integer qtdNumeros, Integer[] numeros) {
 
-        Integer[][] matriz = new Integer[numeros.size ( )][numeros.size ( )];
-        int aux = 0;
+        boolean matrizQuadrada = true;
 
-        for (int i = 0; i < numeros.size ( ); i++) {
-            for (int j = 0; j < numeros.size ( ); j++) {
-
-                if(i > j){
-                    if (numeros.get ( j ) == null ){
-                        matriz[i][j] = 0;
-                    }else{
-                        matriz[i][0] = numeros.get ( i);
-                        matriz[0][j] = numeros.get ( j);
-                    }
-                }
+        Integer tamanho = numeros.length;
+        for(Integer linha : numeros){
+            if (tamanho != linha) {
+                matrizQuadrada = false;
+                break;
             }
         }
 
-        for (int i = 0; i < numeros.size ( ); i++) {
-            for (int j = 0; j < numeros.size ( ); j++) {
-                System.out.print ( matriz[i][j] + " " );
+        System.out.println ( "A matriz é quadrada? " + matrizQuadrada );
+
+    }
+
+    //create method to print square matrix
+    public static void imprimeMatriz(Integer[][] matriz) {
+        for (Integer[] integers : matriz) {
+            for (Integer integer : integers) {
+                System.out.print ( integer + " " );
             }
             System.out.println ( );
         }
     }
-
-    //verify if arraylist form a square matrix
-    public static boolean isSquareMatrix(ArrayList<Integer> numeros) {
-
-        int linha = 0;
-        int coluna = 0;
-
-        for (int i = 0; i < numeros.size ( ); i++) {
-            linha++;
-            System.out.println ( "Linha: " + linha );
-            for (int j = 0; j < numeros.size ( ); j++) {
-                coluna++;
-                System.out.println ( "Coluna: " + coluna );
-            }
-        }
-
-        if (linha != coluna) {
-            return false;
-        }
-        return true;
-    }
-
 
 }
