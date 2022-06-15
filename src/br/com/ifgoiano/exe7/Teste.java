eb786098ff8f87414046e4a4f7b70a44f8a80b36
Carlos Cesar Nunes Bento <carloscesar_nunes@hotmail.com>
2022-06-14 22:17:01 -0300

    exe7

--- src/br/com/ifgoiano/exe7/Teste.java
+++ src/br/com/ifgoiano/exe7/Teste.java
@@ -1,5 +1,7 @@
 package br.com.ifgoiano.exe7;
 
+import java.util.Scanner;
+
 // 7. Escreva um programa que leia:
 // ●   a quantidade de números que deverá processar;
 // ●   os números que deverão ser processados, calcule e exiba, para cada número a ser  processado, o 
@@ -8,4 +10,28 @@ package br.com.ifgoiano.exe7;
 
 public class Teste {
 
+  public static void main(String args[]) {
+    Scanner scanner = new Scanner(System.in);
+
+    int quantidadeNumeros;
+    Double resultado = 1.0;
+
+    System.out.print("Digite a quantidade de numeros a serem processados: ");
+    quantidadeNumeros = scanner.nextInt();
+    Double[] numeros = new Double[quantidadeNumeros];
+
+    for (int x = 0; x < numeros.length; x++) {
+      System.out.print("Digite um numero: ");
+      numeros[x] = scanner.nextDouble();
+    }
+
+    for (int x = 0; x < numeros.length; x++) {
+      for (Double y = numeros[x]; y > 0; y--) {
+        resultado = y * resultado;
+      }
+      System.out.println("O fatorial de " + numeros[x] + " e: " + resultado);
+      System.out.printf("O fatorial de %.0f eh igual a %.2f %n", numeros[x], resultado);
+    }
+
+  }
 }
