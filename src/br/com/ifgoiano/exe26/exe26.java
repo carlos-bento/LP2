892f4ce6d16c021db5d8f703bacf20726b9f87ee
MarcosPauloSR <marcospaulosrezende@gmail.com>
2022-06-15 20:46:11 -0300

    Exercício 26

---
+++ src/br/com/ifgoiano/exe26/exe26.java
@@ -0,0 +1,45 @@
+package br.com.ifgoiano.exe26;
+
+import java.util.Scanner;
+
+public class exe26 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner ( System.in );
+        System.out.println ( "Informe o tamanho da matriz quadrada: " );
+        int tamanho = sc.nextInt ( );
+
+        Boolean[][] matriz = new Boolean[tamanho][tamanho];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.printf ( "Insira o valor da posição: m[%d][%d]: ", (i + 1), (j + 1) );
+                matriz[i][j] = sc.nextBoolean ( );
+            }
+        }
+
+        System.out.println ( "Matriz Original: " );
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.printf ( "%b  ", matriz[i][j] );
+            }
+            System.out.print ( "\n" );
+        }
+
+        System.out.println ( "Matriz resultante" );
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz.length; j++){
+                if(matriz[i][j] == matriz[j][i] ){
+                    matriz[i][j] = true;
+                }else{
+                    matriz[i][j] = false;
+                }
+                System.out.print ( matriz[i][j] + "  " );
+            }
+            System.out.print ( "\n" );
+        }
+
+    }
+
+
+}
