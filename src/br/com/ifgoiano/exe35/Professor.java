c0feec12628629e72342471bc7b8c26cd2f13d4d
MarcosPauloSR <marcospaulosrezende@gmail.com>
2022-06-16 09:34:57 -0300

    Exercício 35

---
+++ src/br/com/ifgoiano/exe35/Professor.java
@@ -0,0 +1,14 @@
+package br.com.ifgoiano.exe35;
+
+public class Professor extends Pessoa {
+
+    private String disciplina;
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+}
