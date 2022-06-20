e2c5edbfbbc8af3577784fb613d81ef961d0641a
Carlos Cesar Nunes Bento <carloscesar_nunes@hotmail.com>
2022-06-20 17:36:47 -0300

    correcao exe 11

--- src/br/com/ifgoiano/exe11/Teste.java
+++ src/br/com/ifgoiano/exe11/Teste.java
@@ -4,42 +4,25 @@ import java.util.Scanner;
 
 public class Teste {
 
-    // 11. A Secretaria de Meio Ambiente que controla o índice de poluição mantém 3
-    // grupos de indústrias que são
-    // altamente poluentes do meio ambiente. O índice de poluição aceitável varia de
-    // 0,05 até 0,25. Se o índice
-    // sobe para 0,3 as indústrias do 1° grupo são intimadas a suspenderem suas
-    // atividades, se o índice crescer
-    // para 0,4 as indústrias do 1°o e 2° grupo são intimadas a suspenderem suas
-    // atividades, se o índice atingir 0,5
-    // todos os grupos devem ser notificados a paralisarem suas atividades. Faça um
-    // programa que leia o índice
-    // de poluição medido e emita a notificação adequada aos diferentes grupos de
-    // empresas. O algoritmo só deve
-    // parar de rodar quando o usuário responder "S" na seguinte pergunta, "Deseja
-    // encerrar o programa?"
-
     public static void main(String[] args) {
 
-        try (Scanner sc = new Scanner(System.in)) {
-            String sair = "";
+        Scanner sc = new Scanner(System.in);
+        String sair = "";
 
-            while (!(sair).equalsIgnoreCase("S")) {
-                recebeIndice();
-                System.out.println("Deseja sair? (S para sair e N para não)");
-                sair = sc.next();
-            }
+        while (!(sair).equalsIgnoreCase("S")) {
+            recebeIndice();
+            System.out.println("Deseja sair? (S para sair e N para não)");
+            sair = sc.next();
         }
 
     }
 
     public static void recebeIndice() {
         try {
-            try (Scanner sc = new Scanner(System.in)) {
-                System.out.println("Digite o índice de poluíção medido:");
-                Double indice = sc.nextDouble();
-                verificaIndice(indice);
-            }
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Digite o índice de poluíção medido:");
+            Double indice = sc.nextDouble();
+            verificaIndice(indice);
         } catch (Exception e) {
             System.out.println("Erro: o valor digitado deve ser um número! Erro: " + e.getMessage());
             recebeIndice();
