2bea4e1e122d487421adba40160557ccdc2371dd
Carlos Cesar Nunes Bento <carloscesar_nunes@hotmail.com>
2022-06-20 18:27:00 -0300

    correcao exe27

--- src/br/com/ifgoiano/exe27/Teste.java
+++ src/br/com/ifgoiano/exe27/Teste.java
@@ -5,52 +5,55 @@ import java.util.Scanner;
 
 public class Teste {
 
-    //27. Escreva um programa Java para encontrar os 𝑘 maiores elementos em um determinado array
-    //multidimensional. Os elementos na matriz podem estar em qualquer ordem e 𝑘 deve ser informado pelo
-    //usuário.
+    // 27. Escreva um programa Java para encontrar os 𝑘 maiores elementos em um
+    // determinado array
+    // multidimensional. Os elementos na matriz podem estar em qualquer ordem e 𝑘
+    // deve ser informado pelo
+    // usuário.
 
     public static void main(String[] args) {
 
         Scanner ler = new Scanner(System.in);
         int k, i, j, cont = 0, maior;
-        int mat[][]= new int[10][10];
+        int mat[][] = new int[10][10];
         Random gerador = new Random();
 
-        //Gerar 100 numeros inteiros na matriz
-        System.out.printf("Numero gerado na matriz:\n");//Verificar
-        for(i=0;i<10;i++){
-            for(j=0;j<10;j++){
-                mat[i][j] = gerador.nextInt(10);
-                System.out.printf("%d\n", mat[i][j]);//Verificar
-
+        // Gerar 100 numeros inteiros na matriz
+        System.out.printf("Numero gerado na matriz:\n");// Verificar
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                mat[i][j] = gerador.nextInt(100);
+                System.out.printf("%d ", mat[i][j]);// Verificar
             }
+            System.out.print("\n");
         }
 
-        System.out.printf("Quando numeros deseja consultar: ");
+        System.out.printf("Quantos numeros deseja consultar: ");
         k = ler.nextInt();
 
         int vet[] = new int[k];
 
-        for(i=0;i<k;i++){
-            vet[i]=0;
+        for (i = 0; i < k; i++) {
+            vet[i] = 0;
         }
 
-        //Percorrer a matriz k vezes para recuperar os k maiores, preenchendo com -1 os maiores ja encontrados
-        while(cont<k){
+        // Percorrer a matriz k vezes para recuperar os k maiores, preenchendo com -1 os
+        // maiores ja encontrados
+        while (cont < k) {
 
             maior = mat[0][0];
 
-            for(i=0;i<10;i++){
-                for(j=0;j<10;j++){
-                    if(mat[i][j] > maior){
+            for (i = 0; i < 10; i++) {
+                for (j = 0; j < 10; j++) {
+                    if (mat[i][j] > maior) {
                         maior = mat[i][j];
                     }
                 }
             }
 
-            for(i=0;i<10;i++){
-                for(j=0;j<10;j++){
-                    if(mat[i][j] == maior){
+            for (i = 0; i < 10; i++) {
+                for (j = 0; j < 10; j++) {
+                    if (mat[i][j] == maior) {
                         mat[i][j] = -1;
                     }
                 }
@@ -62,11 +65,9 @@ public class Teste {
 
         System.out.printf("Os %d maiores sao:\n", k);
 
-        for(i=0;i<k;i++){
+        for (i = 0; i < k; i++) {
             System.out.printf("%d\n", vet[i]);
         }
-
-
 
     }
 
